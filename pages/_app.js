23f4b38391508bf2463a7d27c70175528e3d81ab
faruqmaulana <faruqchodot21@gmail.com>
2022-03-24 18:18:26 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import Script from "next/script";
 import Layout from "../components/Layout";
 
 import "../styles/globals.css";
@@ -12,9 +13,24 @@ Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps, categories }) {
   return (
-    <Layout categories={categories}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout categories={categories}>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-D66R9L10Y4`}
+        />
+        <Script strategy="lazyOnload">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-D66R9L10Y4');
+        `}
+        </Script>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
